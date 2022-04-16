9ebe06ce90704daea2a4063017c894e225b47c7e
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2022-04-15 21:16:31 -0300

    first commit

---
+++ articles/ArticlesController.js
@@ -0,0 +1,138 @@
+const express = require("express")
+const router = express.Router()
+const Category = require("../categories/Category")
+const Article = require("../articles/Article")
+const slugify = require("slugify")
+const adminAuth = require("../middlewares/adminAuth")
+
+router.get("/admin/articles", adminAuth, (req, res) => {
+
+    Article.findAll({
+        include: [{model: Category}]
+    }).then(articles => {
+
+        res.render("admin/articles/index", {articles: articles})
+    })
+})
+
+router.get("/admin/articles/new", adminAuth, (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", { categories: categories})
+    })
+})
+
+router.post("/articles/save", adminAuth, (req, res) => {
+    var title = req.body.title
+    var body = req.body.body
+    var category = req.body.category
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles")
+    })
+})
+
+router.post("/articles/delete", adminAuth, (req, res) => {
+    var id = req.body.id
+    if(id != undefined){
+        if(!isNaN(id)){
+
+            Article.destroy({
+                where: {
+                    id:id
+                }
+            }).then(() => {
+                res.redirect("/admin/articles")
+            })
+
+        }else{
+            res.redirect("/admin/articles")
+        }
+    }else{
+        res.redirect("/admin/articles")
+    }
+})
+
+router.get("/admin/articles/edit/:id", adminAuth, (req, res) => {
+    var id = req.params.id
+    Article.findByPk(id).then(articles => {
+        if(articles != undefined) {
+            Category.findAll().then(categories => {
+
+                res.render("admin/articles/edit", { categories: categories, articles: articles })
+            })
+        }else{
+            res.redirect("/")
+        }
+    }).catch(err => {
+        res.redirect("/")
+    })
+})
+
+router.post("/articles/update", adminAuth,  (req, res) => {
+    var id = req.body.id
+    var title = req.body.title
+    var body = req.body.body
+    var category = req.body.category
+
+    Article.update({ title: title, body: body, title: title, categoryId: category, slug: slugify(title)}, {
+        where:{
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles")
+    }).catch(err => {
+        res.redirect("/")
+    })
+})
+
+
+router.get("/articles/page/:num", (req, res) => {
+    var page = req.params.num
+
+    var offset = 0 
+
+    if(isNaN(page) || page == 1) {
+        offset = 0
+    }else {
+        offset = (parseInt(page) -1 ) * 2
+    }
+
+    Article.findAndCountAll({
+        limit: 2,
+        offset: offset,
+        order: [
+            ['id', 'DESC']
+        ],
+    }).then(articles => {
+        
+        var next
+
+        console.log(offset)
+
+        console.log(articles.count)
+
+        if(offset + 2 >= articles.count){
+            next = false
+        }else {
+            next = true
+        }
+
+        var result = {
+            page: parseInt(page),
+            next: next,
+            articles: articles
+        }
+
+        Category.findAll().then(categories => {
+            res.render("admin/articles/page", {categories: categories, result: result})
+        })
+
+    })
+})
+
+module.exports = router
